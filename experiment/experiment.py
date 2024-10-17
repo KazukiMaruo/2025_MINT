4153a6b76639a21fe85c56a523eef321021beb2e
Kazuki Maruo <maruokazuki1998@gmail.com>
2024-10-17 16:45:40 +0200

    changed

---
+++ experiment/experiment.py
@@ -0,0 +1,17 @@
+
+import functions
+
+# chose 'audio' or 'visual'
+modality = 'visual'
+# specify the monitor size (px), width (cm), and distance (cm) from the screen. 
+monitor_data = [1440, 900, 30, 50]   # EEG lab: [1920, 1080, 30, 50], my mac: [1440, 900, 30, 50] 
+# specify whether you need trigger or not.
+EEG_Trigger = False
+
+
+if modality == 'audio':
+    functions.run_audio(monitor_data, EEG_Trigger)
+elif modality == 'visual':
+    functions.run_visual(monitor_data, EEG_Trigger)
+
+
