d95ad0a0732bb676003983884d4f4a555c31dd7e
KazukiMaruo <maruokazuki1998@gmail.com>
2024-10-29 19:23:16 +0100

    changed

--- code/preprocess/test.py
+++ code/preprocess/test.py
@@ -10,6 +10,11 @@ import json
 # ~~~~~~~~~~~~~~ Libraries ~~~~~~~~~~~~~~
 
 
+
+################################################################################
+#################### Environment #############################################
+################################################################################
+
 # open parent folder of this script
 code_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(code_directory) 
@@ -27,17 +32,10 @@ print(os.getcwd())
 from utils import create_if_not_exist, download_datashare_dir, update_eeg_headers, make_31_montage, calculate_artificial_channels
 
 
-
 # get arguments
 modality = 'visual'
 session = 1
 print(f"\n\n Processing {modality} EEG session {session} \n\n")
-
-# create directories
-""" create_if_not_exist(f"{BASE_DIR}/data/raw/{modality}/session_{session}") #  
-create_if_not_exist(f"{BASE_DIR}/data/interim/{modality}/session_{session}")
-create_if_not_exist(f"{BASE_DIR}/data/processed/{modality}/session_{session}") """
-
 
 # download data from datashare
 download_datashare_dir(datashare_dir = os.path.join(DATASHARE_RAW_FOLDER, modality, 'sub-01_ses-01'),
@@ -47,3 +45,11 @@ download_datashare_dir(datashare_dir = os.path.join(DATASHARE_RAW_FOLDER, modali
 
 # get eeg headers from datashare
 update_eeg_headers(f"{BASE_DIR}/data/raw/{modality}/sub-01_ses-01.vhdr") # BrainVision file format commonly used for EEG data.a
+
+
+
+
+################################################################################
+#################### Loading data #############################################
+################################################################################
+
