2244a337f44b8aefb69560d1981765621d0b62e4
KazukiMaruo <maruokazuki1998@gmail.com>
2024-10-29 19:21:43 +0100

    changed

--- code/preprocess/test.py
+++ code/preprocess/test.py
@@ -11,6 +11,8 @@ import json
 
 
 # open parent folder of this script
+code_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(code_directory) 
 os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # import variables from config.json
@@ -19,8 +21,11 @@ with open("config.json") as f: # The file object is referenced as f during the b
 globals().update(config)
 sys.path.append(BASE_DIR) # import custom python packages
 
+print(os.getcwd())
+
 # import cusom functions
 from utils import create_if_not_exist, download_datashare_dir, update_eeg_headers, make_31_montage, calculate_artificial_channels
+
 
 
 # get arguments
@@ -35,10 +40,10 @@ create_if_not_exist(f"{BASE_DIR}/data/processed/{modality}/session_{session}") "
 
 
 # download data from datashare
-download_datashare_dir(datashare_dir = DATASHARE_RAW_FOLDER,
-                       target_dir = os.path.join({BASE_DIR}, 'data'), #  "BASE_DIR": "/u/kazma/MINT/",
+download_datashare_dir(datashare_dir = os.path.join(DATASHARE_RAW_FOLDER, modality, 'sub-01_ses-01'),
+                       target_dir = os.path.join(BASE_DIR, 'data', 'raw', modality), #  "BASE_DIR": "/u/kazma/MINT/",
                        datashare_user = DATASHARE_USER) # "DATASHARE_USER": "kazma",
 
 
 # get eeg headers from datashare
-# update_eeg_headers(f"{BASE_DIR}/data/raw/{session}/{session}.vhdr") # BrainVision file format commonly used for EEG data.a
+update_eeg_headers(f"{BASE_DIR}/data/raw/{modality}/sub-01_ses-01.vhdr") # BrainVision file format commonly used for EEG data.a
