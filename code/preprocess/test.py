0398deb15696c9ef6d0da9daa3d0ebff5810d1a1
KazukiMaruo <maruokazuki1998@gmail.com>
2024-10-31 14:36:02 +0100

    changed

--- code/preprocess/test.py
+++ code/preprocess/test.py
@@ -124,17 +124,116 @@ raw.drop_channels(['Fp1']) # former eye channel (dummy name)
 
 # save
 raw.save(f"{interim_target_dir_path}/raw.fif", overwrite=True) # .fif file, which is a standard file format used in MNE for storing EEG/MEG data.
-# ~~~~~~~~~~~~~~ print and save
+# ~~~~~~~~~~~~~~ print and save ~~~~~~~~~~~~~~
+
 
 
 ################################################################################
 #################### Preprocessing #############################################
 ################################################################################
 
-# band-pass filter
+# ~~~~~~~~~~~~~~ band-pass filter
 raw.filter(l_freq=PREPROC_PARAMS["hpf"], # high-pass
            h_freq=PREPROC_PARAMS["lpf"], # low-pass
            method='fir', #  Finite Impulse Response (FIR) filter 
            fir_design='firwin', # a popular window method for designing FIR filters
            skip_by_annotation='EDGE boundary', # This skips data points marked by annotations such as "EDGE" or "boundary"
            n_jobs=-1) # This allows the filtering to be done in parallel, using all available CPU cores
+# ~~~~~~~~~~~~~~ band-pass filter  ~~~~~~~~~~~~~~
+
+
+
+# ~~~~~~~~~~~~~~ eye movement correction
+if PREPROC_PARAMS["emc"] == "True":
+    filt_raw = raw.copy().filter(l_freq=1.0, h_freq=None, n_jobs=-1)
+    
+    # ICA model to the filtered EEG data
+    ica = ICA(n_components=20, # set the number of ICA to estimate
+              max_iter="auto", # Automatically determines the maximum number of iterations for ICA convergence.
+              method='picard', # This specifies the ICA algorithm to use. Picard is an efficient ICA algorithm suitable for EEG data.
+              random_state=97) # Sets a random seed for reproducibility.
+    ica.fit(filt_raw) # bads seem to be ignored by default
+
+    # automatic detection of EOG/EMG components
+    ica.exclude = []
+    # find which ICs match the EOG pattern
+    indices, scores = ica.find_bads_eog(raw)
+    print(f'Found {len(indices)} independent components correlating with EOG signal.')
+    ica.exclude.extend(indices) # Adds the EOG-correlated ICs to the exclude list. These components will later be removed from the data.
+
+    # barplot of ICA component "EOG/EMG match" scores
+    f = ica.plot_scores(scores,
+                    title=f'IC correlation with EOG',
+                    show=True)
+    f.savefig(f"{interim_target_dir_path}/ica_scores.png", dpi=100)
+
+    # plot diagnostics
+    if indices: # only if some components were found to correlate with EOG/EMG
+        g = ica.plot_properties(raw, 
+                                picks=indices, 
+                                show=False)
+        for gi, p in zip(g, indices):
+            gi.savefig(f"{interim_target_dir_path}/ica_diagnostics_ic{p}.png", dpi=100)
+    plt.close('all')
+
+    # This applies the ICA solution to the raw EEG data,
+    ica.apply(raw)
+# ~~~~~~~~~~~~~~ eye movement correction ~~~~~~~~~~~~~~
+
+
+# ~~~~~~~~~~~~~~ reference 
+raw.set_eeg_reference(PREPROC_PARAMS["ref"], projection=False) #  average of all electrodes is used as the reference. This indicates that the reference should be applied directly
+# ~~~~~~~~~~~~~~ reference ~~~~~~~~~~~~~~
+
+
+# ~~~~~~~~~~~~~~ epoching
+epochs = mne.Epochs(raw, # The continuous EEG data
+                    events, # This is an array of events detected in the EEG data
+                    event_id=EVENT_ID, #event_id,
+                    tmin=PREPROC_PARAMS["base"][0], # the start time = -100ms
+                    tmax=PREPROC_PARAMS["tmax"], # The end time of each epoch = 1s
+                    baseline=tuple(PREPROC_PARAMS["base"]), # (-100 - 0), Baseline correction subtracts the mean signal in this window from the rest of the epoch to remove slow drifts or offset in the data.
+                    detrend=PREPROC_PARAMS["det"], # it removes any linear trends (slow drifts) in the data.
+                    proj=False,
+                    reject_by_annotation=False, 
+                    preload=True)
+# ~~~~~~~~~~~~~~ epoching ~~~~~~~~~~~~~~
+
+
+
+# ~~~~~~~~~~~~~~  autoreject
+if PREPROC_PARAMS["ar"] != "False":
+    
+    if PREPROC_PARAMS["ar"] == "interpolate":        
+        n_interpolate=[len(epochs.info['ch_names'])] # This means the maximum number of channels (equal to the number of channels in the data) will be interpolated, which effectively interpolates all bad channels.
+        consensus=[len(epochs.info['ch_names'])]  # Similarly, the consensus parameter is set to match the number of channels
+    elif PREPROC_PARAMS["ar"] == "interpolate_reject":    
+        n_interpolate = [4, 8, 12, 16]
+        consensus = np.linspace(0, 1.0, 11)
+    else:
+        print("ERROR: No valid AR method provided in config.json, exiting!")
+        sys.exit(1)
+    
+    # automated estimation of rejection threshold based on channel and trial per participant
+    ar = AutoReject(n_interpolate=n_interpolate, # The number of channels to interpolate if they are detected as noisy.
+                    consensus=consensus, # The proportion of channels that must agree to mark a trial as noisy.
+                    random_state=11, # Ensures the results are reproducible by setting the random seed
+                    n_jobs=-1, 
+                    verbose=False) # Suppresses the printing of verbose output.
+    ar.fit(epochs)  # The autoreject algorithm is fitted to the epochs, analyzing the data to determine which trials or channels are noisy.
+    epochs, reject_log = ar.transform(epochs.copy(), return_log=True) # epochs = The cleaned EEG epochs after autoreject has been applied. reject_log = A log of which trials were rejected or which channels were interpolated.
+
+    # plot the rejection log and save plot
+    rej_plot = reject_log.plot('horizontal', show=True)
+    rej_plot.savefig(f"{interim_target_dir_path}/autoreject.png", dpi=100)
+    reject_log.save(f"{interim_target_dir_path}/autoreject.npz", overwrite=True) # must end with .npz, compressed numpy file format
+# ~~~~~~~~~~~~~~  autoreject  ~~~~~~~~~~~~~~ 
+
+
+# ~~~~~~~~~~~~~~ delete surplus trials: SKIP
+
+
+# ~~~~~~~~~~~~~~ save epochs
+epochs.save(f"{interim_target_dir_path}/epochs-epo.fif", overwrite=True)
+print("epochs are saved")
+# ~~~~~~~~~~~~~~save epochs ~~~~~~~~~~~~~~
