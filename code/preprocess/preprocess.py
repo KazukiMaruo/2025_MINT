c0bb7593a76f43057d839bba28fde2739d3735bc
KazukiMaruo <maruokazuki1998@gmail.com>
2024-11-01 11:41:37 +0100

    changed

--- code/preprocess/preprocess.py
+++ code/preprocess/preprocess.py
@@ -1,4 +1,3 @@
-
 # ~~~~~~~~~~~~~~ Libraries
 import sys, os
 import mne # Python package for processing and analyzing electrophysiological data
@@ -12,115 +11,120 @@ import json
 
 
 
-################################################################################
-#################### Environment #############################################
-################################################################################
+#############################################################################
+#################### ENVIROMENT #############################################
+#############################################################################
 
-# open parent folder of this script
+# ~~~~~~~~~~~~~~ open json paramete files and import functions
+code_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(code_directory) 
 os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# import variables from config.json
-with open("config.json") as f: # The file object is referenced as f during the block.
+with open("config.json") as f: # import variables from config.json
     config = json.load(f) 
 globals().update(config)
 sys.path.append(BASE_DIR) # import custom python packages
-
-# import cusom functions
+print(os.getcwd())
 from utils import create_if_not_exist, download_datashare_dir, update_eeg_headers, make_31_montage, calculate_artificial_channels
+# ~~~~~~~~~~~~~~ open json paramete files and import functions ~~~~~~~~~~~~~~
 
-# DEBUG
-#session="sub-001_ses-001"
 
-# get arguments
-if len(sys.argv) != 2:
-   print("Usage: python script.py session")
-   sys.exit(1)
-else:
-   session = sys.argv[1]
-print(f"\n\n Processing session {session} \n\n")
 
+# ~~~~~~~~~~~~~~ Pre-processing Parameters
+modality = 'visual'
+session = 1
+sub_name = 'sub-01_ses-01'
+print(f"\n\n Processing {modality} EEG session {session} of {sub_name}\n\n")
+# ~~~~~~~~~~~~~~ Pre-processing Parameters ~~~~~~~~~~~~~~
+
+
+# ~~~~~~~~~~~~~~ Path settings and make folders
+datashare_dir_path = os.path.join(DATASHARE_RAW_FOLDER, modality, sub_name) #  "DATASHARE_RAW_FOLDER": "MINT/raw/",
 # create directories
-create_if_not_exist(f"{BASE_DIR}/data/raw/{session}") #  "BASE_DIR": "/u/kazma/MINT/",
-create_if_not_exist(f"{BASE_DIR}/data/interim/{session}")
-create_if_not_exist(f"{BASE_DIR}/data/processed/{session}")
+raw_target_dir_path = os.path.join(BASE_DIR, 'data', 'raw', modality, sub_name)
+interim_target_dir_path = os.path.join(BASE_DIR, 'data', 'interim', modality, sub_name)
+processed_target_dir_path = os.path.join(BASE_DIR, 'data', 'processed', modality, sub_name)
+create_if_not_exist(raw_target_dir_path) 
+create_if_not_exist(interim_target_dir_path)
+create_if_not_exist(processed_target_dir_path) 
+# the interested file name
+target_file_name = f"{raw_target_dir_path}/{sub_name}.vhdr"
+# ~~~~~~~~~~~~~~ Path settings and make folders ~~~~~~~~~~~~~~
 
-# download data from datashare
-download_datashare_dir(datashare_dir = f"{DATASHARE_RAW_FOLDER}eeg/{session}", # "DATASHARE_RAW_FOLDER": "MINT/raw/",
-                       target_dir = f"{BASE_DIR}data/raw/{session}", #  "BASE_DIR": "/u/kazma/MINT/",
+
+# ~~~~~~~~~~~~~~ load data from datashare
+download_datashare_dir(datashare_dir = datashare_dir_path,
+                       target_dir = raw_target_dir_path, 
                        datashare_user = DATASHARE_USER) # "DATASHARE_USER": "kazma",
-
 # get eeg headers from datashare
-update_eeg_headers(f"{BASE_DIR}/data/raw/{session}/{session}.vhdr") # BrainVision file format commonly used for EEG data.
-# ~~~~~~~~~~~~~~ Environment set up ~~~~~~~~~~~~~~
+update_eeg_headers(target_file_name) 
+# ~~~~~~~~~~~~~~ load data from datashare ~~~~~~~~~~~~~~
 
 
+###############################################################################
+#################### LOADING DATA #############################################
+###############################################################################
 
-################################################################################
-#################### Loading data #############################################
-################################################################################
 
-raw = mne.io.read_raw_brainvision(f"{BASE_DIR}/data/raw/{session}/{session}.vhdr",  # Brain vision file format contains three files .vhdr for the header, .eeg for the data, and .vmrk for events
+# ~~~~~~~~~~~~~~ Load data and the data ajustments
+raw = mne.io.read_raw_brainvision(target_file_name,  # Brain vision file format contains three files .vhdr for the header, .eeg for the data, and .vmrk for events
                                   misc='auto', # misc = Specifies that channels labeled as miscellaneous (e.g., auxiliary or non-EEG channels) should be automatically detected.
                                   scale=1.0, # 1.0 means no change for scale
                                   preload=True, # the data is loaded directly into memory. This allows for faster processing 
                                   verbose=False) # Suppresses output during the loading process (helpful when you don’t want too much logging information
- 
-# create custom montage
 raw.rename_channels({'VP': 'Fp2',  # In this case, the channel labeled VP is renamed to Fp2, and VM is renamed to Fp1.
                      'VM': 'Fp1'})
-raw.set_montage(make_31_montage(raw)) # A montage is a spatial configuration that specifies the positions of the EEG electrodes on the scalp. # Without this montage, the EEG data would lack spatial information, which is necessary for topographical analyses or visualizations.
+raw.set_montage(make_31_montage(raw))
 
-# annotations / events
+# Trigger value is converted to condition label name (e.g., from 106 to 6_con_totaldot)
 raw.annotations.description = np.array(
     [
         CONDITION_MAP[i] if i in CONDITION_MAP else i
         for i in raw.annotations.description
     ]
 )
-# annotations / duration of each event
+# interested duration of each trial (e.g., 800ms after the onset of image)
 raw.annotations.duration = np.array([POSTSTIM_WINDOW for _ in raw.annotations.description])
 
 # delete remaining stimulus events / This part of the code removes specific types of events from the annotations in the EEG data
 indices_to_remove = [i for i, j in enumerate(raw.annotations.description) if "Stimulus" in j or "Segment" in j or "actiCAP" in j]
 raw.annotations.delete(indices_to_remove)
 
-# error correction in first 3 pilots
-if session in ["sub-301_ses-001", "sub-303_ses-001", "sub-304_ses-001"]:
-    raw.annotations.description = np.roll(raw.annotations.description, 1) # This shifts the annotation descriptions forward by 1 position, which corrects an error where the annotations may have been misaligned. Example: If the original descriptions are [A, B, C], after rolling by 1, they become [C, A, B].
-    
-# get events and event_id
+# get events (number of events, 3), each row represents [event time, previous_event, event_id]
 events, event_id = mne.events_from_annotations(raw, event_id=EVENT_ID)
 
 # resample
 raw, events = raw.resample(SFREQ, events = events) # raw.resample(SFREQ, events=events): Resamples the raw EEG data to a new sampling frequency (SFREQ) while adjusting the timing of the events accordingly.
 raw.events = events #  This assigns the resampled events back to the raw object, so the events remain associated with the data after resampling.
+# ~~~~~~~~~~~~~~ Load data and the data ajustments ~~~~~~~~~~~~~~
 
-# print the number of events per condition
+
+
+# ~~~~~~~~~~~~~~ print and save
+# print the number of trials for each condition
 event_counts = {}
 for key in EVENT_ID.keys():
     event_counts[key] = len(events[events[:, 2] == EVENT_ID[key]])
     print(key, len(events[events[:, 2] == EVENT_ID[key]]))
 
-# This saves the event counts to a JSON file.
-with open(f"{BASE_DIR}/data/interim/{session}/event_counts_before_drop.json", "w") as f:
+# Save event counts in jsonfile
+with open(f"{interim_target_dir_path}/event_counts_before_drop.json", "w") as f:
     json.dump(event_counts, f)
 
 # This saves the events and the event ID mapping as .npy (NumPy binary) files for future use.
-np.save(f"{BASE_DIR}/data/interim/{session}/events.npy", events) # This function saves the array or object
-np.save(f"{BASE_DIR}/data/interim/{session}/event_id.npy", EVENT_ID)
+np.save(f"{interim_target_dir_path}/events.npy", events) # This function saves the array or object
+np.save(f"{interim_target_dir_path}/event_id.npy", EVENT_ID)
 
 # This plots the events and saves the visualization as a .png image.
 fig = mne.viz.plot_events(events, event_id=EVENT_ID, sfreq=raw.info['sfreq'], first_samp=raw.first_samp, show=False)
-fig.savefig(f"{BASE_DIR}/data/interim/{session}/events.png")
-
+fig.savefig(f"{interim_target_dir_path}/events.png")
 
 # creates new artificial channels representing eye movements, using specific EEG channel pairs.
 raw = calculate_artificial_channels(raw.copy(), pairs=[['Fp1', 'Fp2'],['F9', 'F10']], labels=['eyeV', 'eyeH']) # ['Fp1', 'Fp2']: These are the frontal electrodes typically used to capture vertical eye movements (denoted as eyeV in this case). ['F9', 'F10']: These electrodes are often placed near the eyes and can be used to capture horizontal eye movements (denoted as eyeH).
 raw.drop_channels(['Fp1']) # former eye channel (dummy name)
 
 # save
-raw.save(f"{BASE_DIR}/data/interim/{session}/raw.fif", overwrite=True) # .fif file, which is a standard file format used in MNE for storing EEG/MEG data.
-# ~~~~~~~~~~~~~~ Loading data ~~~~~~~~~~~~~~
+raw.save(f"{interim_target_dir_path}/raw.fif", overwrite=True) # .fif file, which is a standard file format used in MNE for storing EEG/MEG data.
+# ~~~~~~~~~~~~~~ print and save ~~~~~~~~~~~~~~
 
 
 
@@ -128,15 +132,18 @@ raw.save(f"{BASE_DIR}/data/interim/{session}/raw.fif", overwrite=True) # .fif fi
 #################### Preprocessing #############################################
 ################################################################################
 
-# band-pass filter
+# ~~~~~~~~~~~~~~ band-pass filter
 raw.filter(l_freq=PREPROC_PARAMS["hpf"], # high-pass
            h_freq=PREPROC_PARAMS["lpf"], # low-pass
            method='fir', #  Finite Impulse Response (FIR) filter 
            fir_design='firwin', # a popular window method for designing FIR filters
            skip_by_annotation='EDGE boundary', # This skips data points marked by annotations such as "EDGE" or "boundary"
            n_jobs=-1) # This allows the filtering to be done in parallel, using all available CPU cores
+# ~~~~~~~~~~~~~~ band-pass filter  ~~~~~~~~~~~~~~
 
-# eye movement correction
+
+
+# ~~~~~~~~~~~~~~ eye movement correction
 if PREPROC_PARAMS["emc"] == "True":
     filt_raw = raw.copy().filter(l_freq=1.0, h_freq=None, n_jobs=-1)
     
@@ -158,7 +165,7 @@ if PREPROC_PARAMS["emc"] == "True":
     f = ica.plot_scores(scores,
                     title=f'IC correlation with EOG',
                     show=True)
-    f.savefig(f"{BASE_DIR}/data/interim/{session}/ica_scores.png", dpi=100)
+    f.savefig(f"{interim_target_dir_path}/ica_scores.png", dpi=100)
 
     # plot diagnostics
     if indices: # only if some components were found to correlate with EOG/EMG
@@ -166,17 +173,20 @@ if PREPROC_PARAMS["emc"] == "True":
                                 picks=indices, 
                                 show=False)
         for gi, p in zip(g, indices):
-            gi.savefig(f"{BASE_DIR}/data/interim/{session}/ica_diagnostics_ic{p}.png", dpi=100)
+            gi.savefig(f"{interim_target_dir_path}/ica_diagnostics_ic{p}.png", dpi=100)
     plt.close('all')
 
     # This applies the ICA solution to the raw EEG data,
     ica.apply(raw)
+# ~~~~~~~~~~~~~~ eye movement correction ~~~~~~~~~~~~~~
 
 
-# reference determines the baseline
+# ~~~~~~~~~~~~~~ reference 
 raw.set_eeg_reference(PREPROC_PARAMS["ref"], projection=False) #  average of all electrodes is used as the reference. This indicates that the reference should be applied directly
+# ~~~~~~~~~~~~~~ reference ~~~~~~~~~~~~~~
 
-# epoching
+
+# ~~~~~~~~~~~~~~ epoching
 epochs = mne.Epochs(raw, # The continuous EEG data
                     events, # This is an array of events detected in the EEG data
                     event_id=EVENT_ID, #event_id,
@@ -187,8 +197,11 @@ epochs = mne.Epochs(raw, # The continuous EEG data
                     proj=False,
                     reject_by_annotation=False, 
                     preload=True)
+# ~~~~~~~~~~~~~~ epoching ~~~~~~~~~~~~~~
 
-# autoreject
+
+
+# ~~~~~~~~~~~~~~  autoreject
 if PREPROC_PARAMS["ar"] != "False":
     
     if PREPROC_PARAMS["ar"] == "interpolate":        
@@ -212,49 +225,15 @@ if PREPROC_PARAMS["ar"] != "False":
 
     # plot the rejection log and save plot
     rej_plot = reject_log.plot('horizontal', show=True)
-    rej_plot.savefig(f"{BASE_DIR}/data/interim/{session}/autoreject.png", dpi=100)
-    reject_log.save(f"{BASE_DIR}/data/interim/{session}/autoreject.npz", overwrite=True) # must end with .npz, compressed numpy file format
-    
-    
-    
-# delete surplus trials --> at the end when formed epochs, because if some epoch forming failed (because eeg ended before tmax), then the rebalancing needs to be performed again
-min_trials = np.min(list(event_counts.values())) # Determine the smallest number of trials across all event types
-dropping_seed = 23
-np.random.seed(dropping_seed)
-
-epochs_equalized = []
-for key in EVENT_ID.keys():
-    if len(epochs[key]) > min_trials:
-        n_surplus = len(epochs[key]) - min_trials
-        ind_to_drop = np.random.choice(np.arange(len(epochs[key])), n_surplus, replace=False)     
-        print(f"Dropped trials in {key}: {ind_to_drop}")   
-        epochs_equalized.append(epochs[key].drop(ind_to_drop))
-    else:
-        epochs_equalized.append(epochs[key])        
-
-epochs = mne.concatenate_epochs(epochs_equalized)
-
-    
-epochs.save(f"{BASE_DIR}/data/processed/{session}/epochs-epo.fif", overwrite=True)
+    rej_plot.savefig(f"{interim_target_dir_path}/autoreject.png", dpi=100)
+    reject_log.save(f"{interim_target_dir_path}/autoreject.npz", overwrite=True) # must end with .npz, compressed numpy file format
+# ~~~~~~~~~~~~~~  autoreject  ~~~~~~~~~~~~~~ 
 
 
-################################################################################
-#################### Evoked        #############################################
-################################################################################
+# ~~~~~~~~~~~~~~ delete surplus trials: SKIP
 
-# epochs = mne.read_epochs(f"{BASE_DIR}/data/processed/{session}/epochs-epo.fif")
 
-# evoked = [epochs[i].average(method="mean") for i in EVENT_ID.keys()] # TODO try median
-
-# times=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
-# l_face_roi = ["P7", "P3", "TP9", "CP5"]
-# r_face_roi = ["P8", "P4", "TP10", "CP6"]
-
-# for i, condition in enumerate(EVENT_ID.keys()):
-#     p1 = evoked[i].plot_joint(times=times, title=condition, show=False)
-#     p1.savefig(f"{BASE_DIR}/data/interim/{session}/evoked_{condition}.png", dpi=100)
-
-# for i, condition in enumerate(EVENT_ID.keys()):
-#     p2 = epochs[condition].plot_image(picks=r_face_roi, show=False, title=condition, combine="mean")
-#     p2[0].savefig(f"{BASE_DIR}/data/interim/{session}/epochs_r_face_roi_{condition}.png", dpi=100)
-        
+# ~~~~~~~~~~~~~~ save epochs
+epochs.save(f"{interim_target_dir_path}/epochs-epo.fif", overwrite=True)
+print("epochs are saved")
+# ~~~~~~~~~~~~~~save epochs ~~~~~~~~~~~~~~
